dae4090f32ab96a262b0bf9328e6bab1d0e90b0b
Brian Austin <austin.brian@gmail.com>
2019-07-22 08:01:57 -0400

    Add full readme description to setup

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='grandas',
-      version='0.1',
+      version='0.1dev',
       description='Simple and flexible graph database analysis',
       url='http://github.com/austinbrian/grandas',
       author='Brian Austin',
       author_email='austin.brian+gh@gmail.com',
       license='BSD-3',
+      long_description=open('README.md').read(),
       packages=['grandas'],
       zip_safe=False)
