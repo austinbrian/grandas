0453e033c60c8deeaac61e6ac7ad3a365ff19cb7
Brian Austin <austin.brian@gmail.com>
2019-07-25 17:41:35 -0400

    Outline methods forRelationships and Nodes objects
    Set up as objects that build on pandas DataFrame base classes, and expand with additional methods

--- grandas/graphframe.py
+++ grandas/graphframe.py
@@ -1,5 +1,5 @@
 """Initial graph object"""
-import pandas as pd
+from pandas import DataFrame, Series
 
 from .graph_objects import Node, Relationship
 
@@ -21,17 +21,49 @@ class GraphFrame:
         pass
 
     def make_nodes(self, nodes):
+        # Make this from the Nodes class below
 
-        return pd.Series(nodes)
+        return Series(nodes)
 
     def make_relationships(self):
         pass
 
-    def head(self, n=5):
+    def head(self, n=5, r=15):
         # NOTE: Head should return the top 5 (by default) nodes, and all the
         # relationships associated with those 5 nodes (up to a limit of, say 10?)
 
         # NOTE: This should display two distinct dataframes, so should have some
         # edited settings in jupyter notebook:
         # https://stackoverflow.com/questions/38783027/jupyter-notebook-display-two-pandas-tables-side-by-side
+        pass
+
+
+class Nodes(DataFrame):
+    """Extends a pandas DataFrame to initialize for Node objects in grandas"""
+
+    def __init__(self, nodes: list):
+        super(DataFrame, self).__init__()
+
+        all_columns = list(set([n.keys() for n in nodes]))
+
+
+class Relationships(DataFrame):
+    """Extends a pandas DataFrame to initialize for Relationship objects in grandas"""
+
+    def __init__(self, relationships: list):
+        super(DataFrame, self).__init__()
+
+    def expand(self):
+        """
+        Method takes the nodes that are multi-indexed in Relationships and expands
+        out the properties in each of them, for a dataframe with the full set of
+        all details on the entity-relationship pairs.
+        """
+        pass
+
+    def invert(self, label="label"):
+        """
+        Examines the entities and relationships available based on entity type or
+        another field
+        """
         pass
