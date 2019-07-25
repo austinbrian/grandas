7cf42635187bb80d91472064e938faad1ecb64c4
Brian Austin <austin.brian@gmail.com>
2019-07-25 15:37:45 -0400

    Create basic Node functionality
    Keeps Node objects lightweight, able to be initialized using just a
    dictionary. Here, labels and attributes are equivalent, showing up as
    par of the _attrs attribute. Implements a getter to allow easy indexing
    of these attributes, and a to_json method.

--- grandas/graph_objects.py
+++ grandas/graph_objects.py
@@ -1,9 +1,25 @@
+import json
+
 
 class Node:
-    pass
+    """
+    Nodes are created by passing in a dictionary or JSON object at initialization
+    """
+
+    def __init__(self, **kwargs):
+        self._attrs = dict(**kwargs)
+
+    def __getitem__(self, item: str):
+        return self._attrs[item]
+
+    def to_json(self) -> str:
+        return json.dumps(self._attrs)
+
 
 class Relationship:
+    # NOTE: bidirectected relationships are duplicated as two relationships
     pass
+
 
 class Subgraph:
     pass
