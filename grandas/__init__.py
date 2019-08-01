1df4d05210bfe1f64f746188fcc5ad9d03a287a9
Brian Austin <austin.brian@gmail.com>
2019-08-01 17:22:30 -0400

    Add NodeFrame and RelFrame to init

--- grandas/__init__.py
+++ grandas/__init__.py
@@ -1,4 +1,4 @@
 name = "grandas"
-from .graphframe import GraphFrame
+from .graphframe import GraphFrame, NodeFrame, RelationshipFrame
 from .graph_objects import Node, Relationship, Subgraph
 from .engine import graph_engine
